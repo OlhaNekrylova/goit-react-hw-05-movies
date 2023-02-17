7bd57922ea06c919da0539189e2e38746d82c85d
OlhaNekrylova <nekrylovaurist@gmail.com>
2023-02-17 21:28:26 +0200

    movieDetails

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -2,10 +2,11 @@ import {useState, useEffect, useCallback} from 'react';
 import { Link, Outlet, useParams, useNavigate, useLocation } from 'react-router-dom';
 
 import { getMovieById } from '../../shared/services/movies-api';
+import imageUrl from '../../shared/services/movies-api';
 
 const MovieDetails = () => {
     const [movie, setMovie] = useState();
-    const {id} = useParams();
+    const {movieId} = useParams();
 
     const navigate = useNavigate();
 
@@ -14,28 +15,48 @@ const MovieDetails = () => {
     const from = location.state?.from || "/"; 
 
     useEffect(()=> {
-        const fetchPost = async() => {
+        const fetchMovie = async() => {
             try {
-                const result = await getMovieById(id);
+                const result = await getMovieById(movieId);
                 setMovie(result);
             }
             catch({response}) {
                 console.log(response.data.message);
             }
         }
-        fetchPost();
-    }, [])
+        fetchMovie();
+    }, [movieId])
 
-    const goBack = useCallback(()=> navigate(from), [navigate]);
+    const goBack = useCallback(()=> navigate(from), [from, navigate]);
 
     return (
         <>
             <button onClick={goBack}>Go back</button>
-            <h1>{movie?.title}</h1>
-            <p>{movie?.body}</p>
-            <h2>Additional information</h2>
-            <Link to="cast" state={{from}}>cast</Link>
-            <Link to="reviews" state={{from}}>reviews</Link>
+            {movie && (
+                <>
+                <img src={`${imageUrl}${movie.poster_path}`} alt={movie.title} />
+                <h3>
+                    {movie.title}({movie.release_date.split('-')[0]})
+                </h3>
+                <span>User Score: {movie.vote_average * 10}%</span>
+                <h2>Overview</h2>
+                <span>{movie.overview}</span>
+                {<h3>Genres</h3>}
+                {<span>{movie.genres.map(genre => genre.name).join(' ')}</span>}
+                </>)}
+
+                <hr />
+                <span>Additional information</span>
+
+                <ul>
+                    <li>
+                        <Link to="cast" state={{from}} onClick={makeVisibleCast}>cast</Link>
+                    </li>
+                    <li>
+                        <Link to="reviews" state={{from}} onClick={makeVisibleReviews}>reviews</Link>
+                    </li>
+                </ul>
+                <hr />
             <Outlet />
         </>
     )
