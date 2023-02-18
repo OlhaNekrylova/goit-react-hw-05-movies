c6af6bc7ecb3033f22981ec7cd37e28899a83661
OlhaNekrylova <nekrylovaurist@gmail.com>
2023-02-18 22:59:09 +0200

    MovieDatails

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -39,86 +39,82 @@ const MovieDetails = () => {
     const goBack = useCallback(()=> navigate(from), [from, navigate]);
 
     const {
-        poster_path,
         original_title,
         release_date,
         vote_average,
         overview,
         genres,
         homepage,
-      } = item;
+    } = movie;
     
-      let genresList = '';
-      let date = '';
+    let genresList = '';
+    let date = '';
     
-      if (Object.keys(item).length) {
+    if (Object.keys(movie).length) {
         genresList = genres.map(({ name }, idx) => {
-          if (idx === genres.length - 1) {
+        if (idx === genres.length - 1) {
             return `${name}.`;
-          }
-          return `${name}, `;
+        }
+        return `${name}, `;
         });
     
         date = release_date.slice(0, 4);
-      }
+    }
     
-      return (
+    return (
         <div className="container">
-          {loading && (
-            <div className="loading__container">
-              <TailSpin color="#2196f3" />
+            {loading && (
+                <div className="loading__container">
+                    <TailSpin color="#2196f3" />
+                </div>
+            )}
+            {error && <p>Something goes wrong. Please try again.</p>}
+            <button type="button" onClick={goBack} className="btn">
+                Go back
+            </button>
+            <div className={styles.movie__wrap}>
+                <img
+                    src={imageUrl}
+                    alt=""
+                    width="250"
+                />
+                <div className={styles.info__wrap}>
+                    <h2 className={styles.movie__title}>
+                    <a href={homepage} target="_black">
+                        {original_title} ({date})
+                    </a>
+                </h2>
+                <p className={styles.movie__score}>User score: {vote_average}</p>
+                <h3 className={styles.movie__overviewTitle}>Overview</h3>
+                <p className={styles.movie__overviewText}>{overview}</p>
+                <h3 className={styles.movie__genresTitle}>Genres</h3>
+                <p className={styles.movie__genresList}>{genresList}</p>
+                </div>
             </div>
-          )}
-          {error && <p>Oops. Something goes wrong. Please try again.</p>}
-          <button type="button" onClick={goBack} className="btn">
-            Go back
-          </button>
-          <div className={css.movie__wrap}>
-            <img
-              src={
-                poster_path
-                  ? `https://image.tmdb.org/t/p/original/${poster_path}`
-                  : defaultMovieImg
-              }
-              alt=""
-              width="250"
-            />
-            <div className={css.info__wrap}>
-              <h2 className={css.movie__title}>
-                <a href={homepage} target="_black">
-                  {original_title} ({date})
-                </a>
-              </h2>
-              <p className={css.movie__score}>User score: {vote_average}</p>
-              <h3 className={css.movie__overviewTitle}>Overview</h3>
-              <p className={css.movie__overviewText}>{overview}</p>
-              <h3 className={css.movie__genresTitle}>Genres</h3>
-              <p className={css.movie__genresList}>{genresList}</p>
-            </div>
-          </div>
-          <ul className={css.extraPages__list}>
-            <li className={css.extraPages__item}>
-              <NavLink
-                state={{ from }}
-                to={`/movies/${id}/credits`}
-                className={getClassName}
-              >
-                Cast
-              </NavLink>
-            </li>
-            <li className={css.extraPages__item}>
-              <NavLink
-                state={{ from }}
-                to={`/movies/${id}/reviews`}
-                className={getClassName}
-              >
-                Reviews
-              </NavLink>
-            </li>
-          </ul>
+            <ul className={styles.extraPages__list}>
+                <li className={styles.extraPages__item}>
+                    <NavLink
+                        state={{ from }}
+                        to={`/movies/${movieId}/credits`}
+                        className={getClassName}
+                    >
+                    Cast
+                    </NavLink>
+                </li>
+                <li className={styles.extraPages__item}>
+                    <NavLink
+                        state={{ from }}
+                        to={`/movies/${movieId}/reviews`}
+                        className={getClassName}
+                    >
+                    Reviews
+                    </NavLink>
+                </li>
+            </ul>
     
-          <Outlet />
+            <Outlet />
         </div>
-      );
-    };
+    );
+};
+
 export default MovieDetails;
